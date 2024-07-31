482c739d6113f2d196e7ff402e702d593c5b84d7
Kirill_Kochugin <kraingor@yandex.ru>
2024-07-31 16:59:42 +0300

    eurika_added

--- vkBot/src/main/java/myVkBot/VkSimpleBotApplication.java
+++ vkBot/src/main/java/myVkBot/VkSimpleBotApplication.java
@@ -3,9 +3,9 @@ package myVkBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class VkSimpleBotApplication {
     public static void main(String[] args) {
