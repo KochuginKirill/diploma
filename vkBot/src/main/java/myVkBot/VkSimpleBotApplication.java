55ea6b626faa4f2240c3bc97ab803ed4f43376f2
Kirill_Kochugin <kraingor@yandex.ru>
2024-07-31 16:49:40 +0300

    eurika

--- vkBot/src/main/java/myVkBot/VkSimpleBotApplication.java
+++ vkBot/src/main/java/myVkBot/VkSimpleBotApplication.java
@@ -2,7 +2,10 @@ package myVkBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VkSimpleBotApplication {
     public static void main(String[] args) {
--- webSrore/src/main/java/com/example/bysell/WebStoreApplication.java
+++ webSrore/src/main/java/com/example/bysell/WebStoreApplication.java
@@ -2,12 +2,14 @@ package com.example.bysell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class BysellApplication {
+public class WebStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BysellApplication.class, args);
+		SpringApplication.run(WebStoreApplication.class, args);
 	}
 
 }
